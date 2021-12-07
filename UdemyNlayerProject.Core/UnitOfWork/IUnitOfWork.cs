03eb4ac4ae3a14619088ae8ce0b97313aad92169
dogukan <dogukan.matul@gmail.com>
2021-12-07 19:30:56 +0300

    unitofwork oluşturuldu

---
+++ UdemyNlayerProject.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNlayerProject.Core.Repositories;
+
+namespace UdemyNlayerProject.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+         IProductRepository Products { get;  }
+         ICategoryRepository Categories { get;  }
+        Task CommitAsync();
+        void Commit();
+    }
+}
