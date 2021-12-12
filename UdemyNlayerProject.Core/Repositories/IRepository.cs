96539d6417f6135560a7c84b32839404ed3a4b7f
dogukan <dogukan.matul@gmail.com>
2021-12-12 22:32:41 +0300

    IRepository interfacesi implemente edildi

--- UdemyNlayerProject.Core/Repositories/IRepository.cs
+++ UdemyNlayerProject.Core/Repositories/IRepository.cs
@@ -10,14 +10,14 @@ namespace UdemyNlayerProject.Core.Repositories
     {
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
 
         Task AddAsync(TEntity entity);
         Task AddRangeAsync(IEnumerable<TEntity> entities);
         void Remove(TEntity entity);
         void RemoveRange(IEnumerable<TEntity> entities);
-        void Update(TEntity entity);
+        TEntity Update(TEntity entity);
 
 
     }
