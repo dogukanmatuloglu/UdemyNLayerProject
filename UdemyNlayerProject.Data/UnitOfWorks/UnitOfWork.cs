f9ce23a30c99b930719764e1527614aabd9bb635
dogukan <dogukan.matul@gmail.com>
2021-12-13 17:18:03 +0300

    Unit Of Work Sınıfı Oluşturuldu

---
+++ UdemyNlayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNlayerProject.Core.Repositories;
+using UdemyNlayerProject.Core.UnitOfWork;
+using UdemyNlayerProject.Data.Repository;
+
+namespace UdemyNlayerProject.Data.UnitOfWorks
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+           _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
