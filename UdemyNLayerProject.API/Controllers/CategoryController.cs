c5d8f5b8a37a85fef242d4fdf7b625a6ea0d7a1d
dogukan <dogukan.matul@gmail.com>
2021-12-17 02:10:17 +0300

    CategoryController GetById metodu oluşturuldu

--- UdemyNLayerProject.API/Controllers/CategoryController.cs
+++ UdemyNLayerProject.API/Controllers/CategoryController.cs
@@ -28,5 +28,11 @@ namespace UdemyNLayerProject.API.Controllers
             var categories = await _categoryService.GetAllAsync();
             return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
         }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return Ok(_mapper.Map<CategoryDto>(category));
+        }
     }
 }
