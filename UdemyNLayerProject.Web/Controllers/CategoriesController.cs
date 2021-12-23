0f2df12a5e9025339b4f10829d84149d2b81dbe3
dogukan <dogukan.matul@gmail.com>
2021-12-23 20:37:17 +0300

    MVC CategoriesController Oluşturuldu

---
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNlayerProject.Core.Service;
+
+namespace UdemyNLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public CategoriesController(ICategoryService categoryService,IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
